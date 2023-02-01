6afe09cc77c495b151db08ddd840b0b5c9fe1969
HericMendez <heric.mendez00@gmail.com>
2023-02-01 00:19:57 -0300

    consertado bug do titulo não aparecendo no inicio

--- src/components/Elements/MainContainer/index.jsx
+++ src/components/Elements/MainContainer/index.jsx
@@ -9,25 +9,15 @@ import Skills from "components/Pages/Skills";
 import Contact from "components/Pages/Contact";
 
 import {
-
-
-
   GiBehold,
   GiCycle,
   GiBrazilFlag,
-
   GiDrippingSword,
-  
-
   GiHouse,
-
-
   GiScrollQuill,
   GiSkills,
-
   GiWantedReward,
   GiUsaFlag,
-
 } from "react-icons/gi";
 
 import NavButton from "../NavButton";
@@ -40,11 +30,11 @@ import Text from "../../../context/Languages/MultiLingualContent";
 import Page404 from "components/Pages/Page404";
 
 const MainContainer = () => {
-  const [titleID, setTitleID] = useState("Heric's Portfolio - Main Page");
+  const [titleID, setTitleID] = useState('title_home');
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   const { language, toggleLanguage } = useContext(LanguageContext);
-  
+
   console.log("title:", titleID);
   const ChangeThemeIcon = () =>
     theme === "ff-theme" ? HandCursor : HeartCursor;
@@ -62,7 +52,7 @@ const MainContainer = () => {
     marginRight: 5,
     display: "flex",
     flexDirection: "column",
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
     padding: 10,
     fontSize: 10,
   };
@@ -75,37 +65,40 @@ const MainContainer = () => {
             <li className="nav-item">
               <Link id="title_home" to="/">
                 <img src={ChangeThemeIcon()} alt="" />
-                <Text contentID='nav_home'/>
+                <Text contentID="nav_home" />
               </Link>
             </li>
 
             <li className="nav-item">
               <Link id="title_bio" to="/bio">
-                <img src={ChangeThemeIcon()} alt="" /> <Text contentID='nav_bio'/>
+                <img src={ChangeThemeIcon()} alt="" />{" "}
+                <Text contentID="nav_bio" />
               </Link>
             </li>
 
             <li className="nav-item">
               <Link id="title_skills" to="/skills">
                 <img className="nav-cursor" src={ChangeThemeIcon()} alt="" />{" "}
-                <Text contentID='nav_skills'/>
+                <Text contentID="nav_skills" />
               </Link>
             </li>
 
             <li className="nav-item">
               <Link id="title_quests" to="/quests">
-                <img src={ChangeThemeIcon()} alt="" /> <Text contentID='nav_quests'/>
+                <img src={ChangeThemeIcon()} alt="" />{" "}
+                <Text contentID="nav_quests" />
               </Link>
             </li>
             <li className="nav-item">
               <Link id="title_contact" to="/contact">
                 <img src={ChangeThemeIcon()} alt="" />
-                <Text contentID='nav_contact'/>
+                <Text contentID="nav_contact" />
               </Link>
             </li>
             <li className="nav-item">
               <Link id="title_about" to="/about">
-                <img src={ChangeThemeIcon()} alt="" /> <Text contentID='nav_about'/>
+                <img src={ChangeThemeIcon()} alt="" />{" "}
+                <Text contentID="nav_about" />
               </Link>
             </li>
           </ul>
@@ -122,8 +115,8 @@ const MainContainer = () => {
               type="submit"
               onClick={toggleTheme}
             >
-              <Text contentID='nav_theme'/>: 
-              <br/>
+              <Text contentID="nav_theme" />:
+              <br />
               {theme === "ff-theme" ? "Final Fantasy" : "Undertale"}
             </button>
 
@@ -137,9 +130,9 @@ const MainContainer = () => {
               type="submit"
               onClick={toggleLanguage}
             >
-              <Text contentID='nav_lang'/>: 
-              <br/>
-              {language==='portuguese'? 'PT-BR': 'ENGLISH'}
+              <Text contentID="nav_lang" />:
+              <br />
+              {language === "portuguese" ? "PT-BR" : "ENGLISH"}
             </button>
           </div>
         </div>
@@ -149,7 +142,7 @@ const MainContainer = () => {
 
         <div className="rpgui-container framed content-window">
           <Routes>
-            <Route path="*" element={<Page404/>}/>
+            <Route path="*" element={<Page404 />} />
             <Route path="/" element={<Home />} />
             <Route path="/skills" element={<Skills />} />
             <Route path="/bio" element={<Bio />} />
@@ -167,40 +160,50 @@ const MainContainer = () => {
           </Link>
           <Link to="/skills">
             <NavButton style={navButtonStyle}>
-              <GiDrippingSword size={36} /> <Text contentID='nav_skills'/>
+              <GiDrippingSword size={36} /> <Text contentID="nav_skills" />
             </NavButton>
           </Link>
           <Link to="/bio">
             <NavButton style={navButtonStyle}>
               <GiSkills size={36} />
-              <Text contentID='nav_bio'/>
+              <Text contentID="nav_bio" />
             </NavButton>
           </Link>
           <Link to="/quests">
             <NavButton style={navButtonStyle}>
               <GiWantedReward size={36} />
-              <Text contentID='nav_quests'/>
+              <Text contentID="nav_quests" />
             </NavButton>
           </Link>
           <Link to="/contact">
             <NavButton style={navButtonStyle}>
               <GiScrollQuill size={36} />
-              <Text contentID='nav_contact'/>
+              <Text contentID="nav_contact" />
             </NavButton>
           </Link>
           <Link to="/about">
             <NavButton style={navButtonStyle}>
               <GiBehold size={36} />
-              <Text contentID='nav_about'/>
+              <Text contentID="nav_about" />
             </NavButton>
           </Link>
 
           <div onClick={toggleTheme}>
-            <NavButton style={navButtonStyle}><GiCycle size={36}/><Text contentID='nav_theme'/></NavButton>
+            <NavButton style={navButtonStyle}>
+              <GiCycle size={36} />
+              <Text contentID="nav_theme" />
+            </NavButton>
           </div>
 
           <div onClick={toggleLanguage}>
-            <NavButton style={navButtonStyle}>{language==='portuguese'? <GiBrazilFlag size={36}/> : <GiUsaFlag size={40}/>}<Text contentID='nav_lang'/></NavButton>
+            <NavButton style={navButtonStyle}>
+              {language === "portuguese" ? (
+                <GiBrazilFlag size={36} />
+              ) : (
+                <GiUsaFlag size={40} />
+              )}
+              <Text contentID="nav_lang" />
+            </NavButton>
           </div>
         </div>
       </div>
